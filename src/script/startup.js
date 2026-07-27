67c9b7a11b1f472abde80bad815bcca86c186d19
Yashvardhan Shekhar <yashvardhan.shekhar@joshtechnologygroup.com>
2026-07-27 16:52:50 +0530

    [YS_A1_03]: Made travel point dynamic

--- src/script/startup.js
+++ src/script/startup.js
@@ -17,7 +17,30 @@ let links = [
     },
 ];
 
+<<<<<<< HEAD
 // insert nav links into HTML page dynamically
+=======
+let stats = [
+    {
+        data: '750+',
+        name: 'Holiday Packages',
+    },
+    {
+        data: 100,
+        name: 'Luxury Hotels',
+    },
+    {
+        data: 7,
+        name: 'Premium Airlines',
+    },
+    {
+        data: '2k+',
+        name: 'Happy Customer',
+    },
+];
+
+// insert links into HTML
+>>>>>>> c188f8e ([YS_A1_03]: Made travel point dynamic)
 function insert() {
     let str =
         ' <div class="header__link__top"> <button type="button" class="header__close-btn"> <svg class="header__close-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z"/></svg> </button> ';
@@ -38,6 +61,21 @@ function insert() {
     heading.innerHTML = `Travel <span>top destination</span> of the world`;
     let description = document.querySelector('.description');
     description.innerHTML = `we always make our customer happy by providing <br /> as many choices as possible`;
+
+    let travelPoint = document.querySelector('.travel-point__right__cards');
+    str = '';
+
+    stats.forEach((s) => {
+        str += `<div class="travel-point__right__cards__card">
+                        <div class="travel-point__right__cards__card__data">
+                            ${s.data}
+                        </div>
+                        <div class="travel-point__right__cards__card__text">
+                             ${s.name}
+                        </div>
+                    </div>`;
+    });
+    travelPoint.innerHTML = str;
 }
 // insert data into the HTML page
 insert();
