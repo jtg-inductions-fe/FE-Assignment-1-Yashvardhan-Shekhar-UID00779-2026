59a8e5f65d52a67e85cd7f4a28d673fd78d1a042
Yashvardhan Shekhar <yashvardhan.shekhar@joshtechnologygroup.com>
2026-07-27 16:52:50 +0530

    [YS_A1_03]: Made travel point dynamic

--- src/script/startup.js
+++ src/script/startup.js
@@ -17,9 +17,15 @@ const links = [
     },
 ];
 
+<<<<<<< HEAD
 const stats = [
     {
         data: '500+',
+=======
+let stats = [
+    {
+        data: '750+',
+>>>>>>> c188f8e ([YS_A1_03]: Made travel point dynamic)
         name: 'Holiday Packages',
     },
     {
@@ -36,6 +42,7 @@ const stats = [
     },
 ];
 
+<<<<<<< HEAD
 // insert nav links into HTML
 function insertNavLinks() {
     const div = document.querySelector('.header__links');
@@ -45,6 +52,13 @@ function insertNavLinks() {
                         <svg class="header__close-btn" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M183.1 137.4C170.6 124.9 150.3 124.9 137.8 137.4C125.3 149.9 125.3 170.2 137.8 182.7L275.2 320L137.9 457.4C125.4 469.9 125.4 490.2 137.9 502.7C150.4 515.2 170.7 515.2 183.2 502.7L320.5 365.3L457.9 502.6C470.4 515.1 490.7 515.1 503.2 502.6C515.7 490.1 515.7 469.8 503.2 457.3L365.8 320L503.1 182.6C515.6 170.1 515.6 149.8 503.1 137.3C490.6 124.8 470.3 124.8 457.8 137.3L320.5 274.7L183.1 137.4z"/></svg> 
                     </button> `;
 
+=======
+// insert links into HTML
+function insert() {
+    let str =
+        ' <div class="header__link__top"> <button class="header__close-btn">&#10006;</button> ';
+    let div = document.querySelector('.header__links');
+>>>>>>> c188f8e ([YS_A1_03]: Made travel point dynamic)
     links.forEach((l) => {
         str += `<a class="header__link text-nav-link" href="${l.path}">${l.name}</a>`;
     });
@@ -62,6 +76,21 @@ function insertNavLinks() {
     heading.innerHTML = `Travel <span>top destination</span> of the world`;
     let description = document.querySelector('.description');
     description.innerHTML = `we always make our customer happy by providing <br /> as many choices as possible`;
+
+    let travelPoint = document.querySelector('.travel-point__right__cards');
+    str = '';
+
+    stats.forEach((s) => {
+        str += `<div class="travel-point__right__cards__card">
+                        <div class="travel-point__right__cards__card__data">
+                            ${s.data}
+                        </div>
+                        <div class="travel-point__right__cards__card__text">
+                             ${s.name}
+                        </div>
+                    </div>`;
+    });
+    travelPoint.innerHTML = str;
 }
 <<<<<<< HEAD
 
