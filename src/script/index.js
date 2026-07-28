b702fa84f691b9af905e0d248da7caabb329284a
Yashvardhan Shekhar <yashvardhan.shekhar@joshtechnologygroup.com>
2026-07-28 13:55:15 +0530

    [YS_A1_02]: Fixed PR and updated incremental changes

--- src/script/index.js
+++ src/script/index.js
@@ -14,6 +14,7 @@ hamburgerBtn.addEventListener('click', () => {
     document.querySelector('.header__close-btn').focus();
 });
 
+<<<<<<< HEAD
 document.addEventListener('click', (e) => {
     // button or icon
     if (e.target.matches('.header__close-btn') || e.target.matches('path')) {
@@ -28,6 +29,11 @@ document.addEventListener('keydown', (e) => {
             .querySelector('.header__links')
             .classList.contains('header__links--none')
     ) {
+=======
+closeBtn.addEventListener('click', (e) => {
+    // button or icon
+    if (e.target.matches('.header__close-btn') || e.target.matches('path')) {
+>>>>>>> d3ce191 ([YS_A1_02]: Fixed PR and updated incremental changes)
         toggleHamburger();
     }
 });
